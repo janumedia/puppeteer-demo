dea3cc68f7a3556bde9779bf849a1dee401e9001
janumedia@yahoo.com.sg <Janu@ElCap.local>
2018-10-10 22:24:23 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const puppeteer = require('puppeteer');
+
+(async() => {
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+    await page.goto('https://janumedia.com');
+    await page.screenshot({path: 'screenshot.png'});
+
+    await browser.close();
+})();
