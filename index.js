e22e3f624e08b4e66d4d69b2f16bd6c2501a24d9
janumedia@yahoo.com.sg <janumedia@gmail.com>
2018-10-10 23:03:49 +0800

    test fullPage screenshot

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const puppeteer = require('puppeteer');
+const devices = require('puppeteer/DeviceDescriptors');
 
 (async() => {
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
-    await page.goto('https://janumedia.com');
-    await page.screenshot({path: 'screenshot.png'});
-
+    await page.emulate(devices['iPad']);
+    await page.goto('http://profile.janumedia.com/portfolio', {waitUntil: 'networkidle0'});
+    await page.screenshot({path: 'screenshot.jpg', type: 'jpeg', quality: 60,fullPage: true});
     await browser.close();
 })();
